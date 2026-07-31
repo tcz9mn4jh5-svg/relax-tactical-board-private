1f96e06e1a8aa4e8b7546fe8d05701129cf65b8d
tcz9mn4jh5-svg <okehiro0423@icloud.com>
2026-07-31 23:28:00 +0900

    Add dedicated half-field lineups

--- static/tactical_board/sw.js
+++ static/tactical_board/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "relax-tactical-board-v6";
+const CACHE_NAME = "relax-tactical-board-v7";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
